3d802efeb9a2f478539208ed828ceb1fec93c2e0
Sandesh Adhikari <sandeshadhikari2003@gmail.com>
2025-05-24 16:19:35 +0530

    dropdown fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
             </div>
 
             {/* Sidebar Menu For Small Screens */}
-            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`} >
+            <div className={`absolute top-0 right-0 bottom-0 z-10 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`} >
                 <div className='flex flex-col text-gray-600'>
                     <div onClick={() => setVisble(false)} className='flex items-center gap-4 p-3 '>
                         <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
